52fc1cf89d74133418efbbc53d5f2bc0dbbfb22c
Nuwil <153420007+Nuwil@users.noreply.github.com>
2023-12-10 16:52:37 +0800

    Add files via upload

---
+++ nuwel.js
@@ -0,0 +1,10 @@
+let div = document.getElementById("div");
+
+div.addEventListener("mouseover", () => {
+    div.style.backgroundImage = `url(https://scontent.fmnl9-4.fna.fbcdn.net/v/t1.15752-9/387645449_875060064345248_7979438001815847780_n.png?_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeH0rKhCX-06A6aYa0UjitfDbKCjve5VTxNsoKO97lVPE3t96BNWG90k7NNk81AOzNXgVZpUzRbHjgq54S6xVCo_&_nc_ohc=4SgM3Mtc-qQAX-tTK-z&_nc_ht=scontent.fmnl9-4.fna&oh=03_AdRSNhRq-eK3FrDFIdIIZKBxE4onxWFMn2Fhaf7Vi_6iOg&oe=6583EB21)`
+})
+
+div.addEventListener("mouseleave", () => {
+    div.style.backgroundImage = `url(https://scontent.fcrk2-2.fna.fbcdn.net/v/t1.15752-9/394646072_298700986446616_7185106902745213492_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGatgXvwqe0r1XWEc_sU9097lNbI3g7Gl3uU1sjeDsaXRAwCnvsNPI6hNB39bYHFwHU8Uko0Z5wkkqMO5zbEuQe&_nc_ohc=gXgLqI03LLsAX_5P7gW&_nc_ht=scontent.fcrk2-2.fna&oh=03_AdRRVhkolPPIm3hfkaMBTGSlhpmpikssCO6gYT7ppqAz1g&oe=659CBA7D)`
+})
+
